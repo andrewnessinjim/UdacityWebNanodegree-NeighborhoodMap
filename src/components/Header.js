4e7caaa67c91bfe0e38332a9ff3e5950917c3185
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-11-19 03:06:32 +0530

    feat: Add hamburger menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import menu_icon from '../icons/menu_icon.png'
 
 const Header = () => {
   return (
-    <h1 className="app-header">Sights in London</h1>
+    <header className="app-header">
+      <img src={menu_icon} alt="Search Places"/>
+      <h1 className="app-header-content">Sights in London</h1>
+    </header>
   );
 }
 
