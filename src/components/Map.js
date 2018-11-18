647b47c0c389a8f2dfbde447c6fe017b63dcf5c1
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-11-19 05:19:45 +0530

    feat: Display random map on screen

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { withScriptjs, GoogleMap, withGoogleMap, Marker } from "react-google-maps"
+
+const MapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+));
 
 const Map = () => {
   return (
-    <div className="map-container"></div>
+    <div className="map-container">
+      <MapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2xj7Uf3-5r4zfmDqSmgwwiVEH9QhE0Js"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
   );
 }
 
--- src/components/SearchableList.js
+++ src/components/SearchableList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const SearchableList = (props) => (
-  <ul className={`places-list${props.listVisible ? " places-list__visible" : " places-list__invisible"}`}>
+  <ul
+    className={`places-list${props.listVisible ? " places-list__visible" : " places-list__invisible"}`}
+  >
     {props.items.map(place => (<li key={place.id}>{place.name}</li>))}
   </ul>
 );
