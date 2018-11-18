bb08fa152e6e1887e972995837ad372f85b8cc5d
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-11-19 00:29:22 +0530

    style: Fix indentation

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <ListAndMapContainer/>
+        <Header />
+        <ListAndMapContainer />
       </div>
     );
   }
--- src/components/ListAndMapContainer.js
+++ src/components/ListAndMapContainer.js
@@ -8,14 +8,14 @@ class ListAndMapContainer extends Component {
   }
 
   componentWillMount() {
-    this.setState({places: testData.response.groups.find(item => item.type === "Recommended Places").items})
+    this.setState({ places: testData.response.groups.find(item => item.type === "Recommended Places").items })
   }
 
   render() {
     console.log(this.state.places);
     return (
       <div className="list-map-container">
-        <SearchableList items={this.state.places.map(place => ({name: place.venue.name, id:place.venue.id}))}></SearchableList>
+        <SearchableList items={this.state.places.map(place => ({ name: place.venue.name, id: place.venue.id }))}></SearchableList>
       </div>
     );
   }
