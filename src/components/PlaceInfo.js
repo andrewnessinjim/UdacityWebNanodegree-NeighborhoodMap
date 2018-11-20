795ab6ee49acf071d17c10f00042bee6eeb9d56c
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-11-20 14:25:02 +0000

    feat: Add sample image to InfoWindow

--- src/components/PlaceInfo.js
+++ src/components/PlaceInfo.js
@@ -7,19 +7,22 @@ class PlaceInfo extends Component {
   }
 
   componentDidMount() {
+    const {name, rating, canonicalUrl, bestPhoto} = venueTestData.response.venue;
     const placeInfo = {
-      name: venueTestData.response.venue.name,
-      rating: venueTestData.response.venue.rating,
-      canonicalUrl: venueTestData.response.venue.canonicalUrl
+      name: name,
+      rating: rating,
+      canonicalUrl: canonicalUrl,
+      bestPhotoUrl: `${bestPhoto.prefix}200x150${bestPhoto.suffix}`
     }
     this.setState({ placeInfo });
   }
 
   render() {
-    const {name, rating, canonicalUrl} = this.state.placeInfo;
+    const {name, rating, canonicalUrl, bestPhotoUrl} = this.state.placeInfo;
     return (
       <section>
         <h3>{name}</h3>
+        <img src={bestPhotoUrl}></img>
         <p>Rating: {rating}</p>
         <a href={canonicalUrl} target="_blank" rel="noopener noreferrer">More Details</a>
       </section>
