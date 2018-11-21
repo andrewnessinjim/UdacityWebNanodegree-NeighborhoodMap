c0b7c50d5bb0eb7a0a1f134cc89ad44ca9f2443b
Andrew Nessin <andrewnessinjim@yahoo.com>
2018-11-21 09:33:07 +0000

    feat: Add foursquare attribution

--- src/components/PlaceInfo.js
+++ src/components/PlaceInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import venueTestData from '../test_data/venue_detail'
+import venueTestData from '../test_data/venue_detail';
+import fourscoreIcon from '../icons/foursquare.png';
 
 class PlaceInfo extends Component {
   state = {
@@ -7,24 +8,27 @@ class PlaceInfo extends Component {
   }
 
   componentDidMount() {
-    const {name, rating, canonicalUrl, bestPhoto} = venueTestData.response.venue;
+    const { name, rating, canonicalUrl, bestPhoto } = venueTestData.response.venue;
     const placeInfo = {
       name: name,
       rating: rating,
       canonicalUrl: canonicalUrl,
-      bestPhotoUrl: `${bestPhoto.prefix}200x150${bestPhoto.suffix}`
+      bestPhotoUrl: `${bestPhoto.prefix}150x100${bestPhoto.suffix}`
     }
     this.setState({ placeInfo });
   }
 
   render() {
-    const {name, rating, canonicalUrl, bestPhotoUrl} = this.state.placeInfo;
+    const { name, rating, canonicalUrl, bestPhotoUrl } = this.state.placeInfo;
     return (
-      <section>
-        <h3>{name}</h3>
+      <section className="place-details">
+        <h2>{name}</h2>
         <img src={bestPhotoUrl}></img>
-        <p>Rating: {rating}</p>
-        <a href={canonicalUrl} target="_blank" rel="noopener noreferrer">More Details</a>
+        <div className="rating-and-link-container">
+          <p><strong>Rating:</strong> {rating}</p>
+          <a className="more-details-link" href={canonicalUrl} target="_blank" rel="noopener noreferrer">More Info</a>
+        </div>
+        <img className="fourscore-img" src={fourscoreIcon} alt="Powered By Fourscore"/>
       </section>
     )
   }
